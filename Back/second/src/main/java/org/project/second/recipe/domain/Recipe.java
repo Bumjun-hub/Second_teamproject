381a8bf55828b02130836420959d5e82e3d11e6d
SB96-Lee <silee7575@gmail.com>
2025-05-21 18:10:09 +0900

    saveee

---
+++ Back/second/src/main/java/org/project/second/recipe/domain/Recipe.java
@@ -0,0 +1,19 @@
+package org.project.second.recipe.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // PK
+}
