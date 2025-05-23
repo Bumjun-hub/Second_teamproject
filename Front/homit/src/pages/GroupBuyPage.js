f46770d8b354192cbb9f2b91a93aba4941fb1167
Bumjun-hub <jc971017@gmail.com>
2025-05-23 15:11:45 +0900

    05/23 프론트 구조 수정

---
+++ Front/homit/src/pages/GroupBuyPage.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Section from "../components/Section";
+
+const GroupBuyPage= () => {
+    return (
+        <>
+        <Header />
+        <Section/>
+        <Footer/>
+        </>
+    )
+
+}
+export default GroupBuyPage;
---
+++ Front/homit/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Section from "../components/Section";
+
+const LoginPage= () => {
+    return (
+        <>
+        <Header />
+        <Section/>
+        
+        </>
+    )
+
+}
+export default LoginPage;
