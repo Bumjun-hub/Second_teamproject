9d764c04f44fe96e817631fc60c77f709405a2e4
Bumjun-hub <jc971017@gmail.com>
2025-05-26 16:51:17 +0900

    Bumjun (#16)
    * 05/23 프론트 시작
    
    * 05/23 Front Page 생성
    
    * 커밋 테스트
    
    * 05/23 프론트 구조 수정
    
    * 05/23 구조 다시 수정
    
    * 05/23 front 구조 수정
    
    * 05/23 프론트수정
    
    공동구매 페이지 더미데이터 추가 + 페이지네이션
    
    * 05/26 수정
    
    공동구매 페이지 css
    
    * 05/26 프론트
    
    공동구매 페이지
    - 목록 / 글쓰기 / 상세정보 페이지 제작

--- Front/homit/src/App.js
+++ Front/homit/src/App.js
@@ -7,6 +7,8 @@ import RecipePage from './pages/RecipePage';
 import HotItemPage from './pages/HotItemPage';
 import LoginPage from './pages/LoginPage';
 import Layout from './components/Layout';
+import GroupBuyInfoPage from './pages/GroupBuyInfoPage';
+import GroupBuyWritePage from './pages/GroupBuyWritePage';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/groupbuy" element={<GroupBuyPage />} />
+        <Route path="/groupbuy/info/:id" element={<GroupBuyInfoPage/>}/>
+        <Route path="/groupbuy/write" element={<GroupBuyWritePage/>}/>
         <Route path="/board" element={<BoardPage />} />
         <Route path="/recipe" element={<RecipePage />} />
         <Route path="/popular" element={<HotItemPage />} />
