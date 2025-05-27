6c56f37f4cd693ac5b0700b618484a1bb3384e1b
SB96-Lee <silee7575@gmail.com>
2025-05-27 10:08:41 +0900

    resavee

---
+++ Back/second/src/main/java/org/project/second/member/dto/MypageResponse.java
@@ -0,0 +1,12 @@
+package org.project.second.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MypageResponse {
+    String email;
+}
