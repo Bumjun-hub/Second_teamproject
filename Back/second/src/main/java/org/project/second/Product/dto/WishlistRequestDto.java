92c5096eb071beedf46a36e50b219210d3d74da8
SB96-Lee <silee7575@gmail.com>
2025-05-29 15:48:33 +0900

    add field_community_isNotice

---
+++ Back/second/src/main/java/org/project/second/Product/dto/WishlistRequestDto.java
@@ -0,0 +1,20 @@
+package org.project.second.Product.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class WishlistRequestDto {
+    private String naverProductId;
+    private String name;
+    private String url;
+    private Double price;
+    private String imageUrl;
+    private String category1;
+    private String category2;
+    private String category3;
+    private String category4;
+}
