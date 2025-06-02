9494e4a3a42bbd51f23fffb4b96a03e8ed0fc0bf
ansunhwa <hbo1053@naver.com>
2025-06-02 17:59:31 +0900

    dd

--- Back/second/src/main/java/org/project/second/groupBuy/service/GroupBuyParticipationService.java
+++ Back/second/src/main/java/org/project/second/groupBuy/service/GroupBuyParticipationService.java
@@ -1,13 +1,27 @@
 package org.project.second.groupBuy.service;
 
 import lombok.RequiredArgsConstructor;
+import org.project.second.groupBuy.domain.GroupBuy;
+import org.project.second.groupBuy.repository.GroupBuyParticipationRepository;
+import org.project.second.groupBuy.repository.GroupBuyRepository;
 import org.project.second.member.domain.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class GroupBuyParticipationService {
+
+    private final GroupBuyRepository groupBuyRepository;
+    private final GroupBuyParticipationRepository groupBuyParticipationRepository;
+
     public void getApply(Long groupBuyId, Member member, int quantity) {
-        
+        GroupBuy groupBuy = groupBuyRepository.findById(groupBuyId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 공동구매가 없습니다"));
+
+        if (member == null) {
+            throw new IllegalArgumentException("로그인이 필요한 기능입니다.");
+        }
     }
+
+
 }
