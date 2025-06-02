84d68cd2a58470edc735226d2c5883ea4f19a150
ansunhwa <hbo1053@naver.com>
2025-06-02 17:35:35 +0900

    dd

--- Back/second/src/main/java/org/project/second/groupBuy/controller/GroupBuyParticipationController.java
+++ Back/second/src/main/java/org/project/second/groupBuy/controller/GroupBuyParticipationController.java
@@ -1,6 +1,14 @@
 package org.project.second.groupBuy.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.project.second.groupBuy.domain.GroupBuyParticipation;
+import org.project.second.groupBuy.service.GroupBuyParticipationService;
+import org.project.second.member.config.CustomUserDetails;
+import org.project.second.member.domain.Member;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/groupBuy")
 public class GroupBuyParticipationController {
 
+    private final GroupBuyParticipationService groupBuyParticipationService;
+
+    @PostMapping("/{id}/apply")
+    public ResponseEntity<String> getApply(
+            @PathVariable("id") Long groupBuyId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+            ) {
+        Member member = userDetails.getMember();
+        int quantity = 1;
+        groupBuyParticipationService.getApply(groupBuyId, member, quantity);
+        return ResponseEntity.ok("신청완료");
+    }
 
 }
--- Back/second/src/main/java/org/project/second/groupBuy/service/GroupBuyParticipationService.java
+++ Back/second/src/main/java/org/project/second/groupBuy/service/GroupBuyParticipationService.java
@@ -1,9 +1,13 @@
 package org.project.second.groupBuy.service;
 
 import lombok.RequiredArgsConstructor;
+import org.project.second.member.domain.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class GroupBuyParticipationService {
+    public void getApply(Long groupBuyId, Member member, int quantity) {
+        
+    }
 }
