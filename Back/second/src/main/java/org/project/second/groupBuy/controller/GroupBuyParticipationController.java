cae910fc0adf38acdc946e7af6fd5c646c504ea0
ansunhwa <hbo1053@naver.com>
2025-06-03 18:03:48 +0900

    dd

--- Back/second/src/main/java/org/project/second/groupBuy/controller/GroupBuyParticipationController.java
+++ Back/second/src/main/java/org/project/second/groupBuy/controller/GroupBuyParticipationController.java
@@ -1,16 +1,15 @@
 package org.project.second.groupBuy.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.project.second.groupBuy.domain.GroupBuyParticipation;
+import org.project.second.groupBuy.dto.GroupBuyParticipationDto;
 import org.project.second.groupBuy.service.GroupBuyParticipationService;
 import org.project.second.member.config.CustomUserDetails;
 import org.project.second.member.domain.Member;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +18,7 @@ public class GroupBuyParticipationController {
 
     private final GroupBuyParticipationService groupBuyParticipationService;
 
+    //신청하기
     @PostMapping("/{id}/apply")
     public ResponseEntity<String> getApply(
             @PathVariable("id") Long groupBuyId,
@@ -29,5 +29,42 @@ public class GroupBuyParticipationController {
         groupBuyParticipationService.getApply(groupBuyId, member, quantity);
         return ResponseEntity.ok("신청완료");
     }
+
+    //관리자만 신청리스트 보기
+    @GetMapping("/admin/{groupBuyId}/applyList")
+    public ResponseEntity<List<GroupBuyParticipationDto>> getApplyList(
+            @PathVariable Long groupBuyId
+    ){
+        List<GroupBuyParticipationDto> applyList =
+                groupBuyParticipationService.getApplyList(groupBuyId);
+        return ResponseEntity.ok(applyList);
+    }
+
+    //신청취소하기
+    @DeleteMapping("/{id}/cancel")
+    public ResponseEntity<String> cancelApply (
+            @PathVariable("id") Long groupBuyId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Member member = userDetails.getMember();
+        groupBuyParticipationService.cancelApply(groupBuyId, member);
+        return ResponseEntity.ok("신청이 취소되었습니다");
+    }
+
+    //내 신청목록만 보기
+    @GetMapping("/myPage")
+    public ResponseEntity<List<GroupBuyParticipationDto>> myParticipationList(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ){
+        Member member = userDetails.getMember();
+        List<GroupBuyParticipationDto> participationList
+                = groupBuyParticipationService.ParticipationList(member);
+
+        return ResponseEntity.ok(participationList);
+    }
+
+
+    //내신청목록상세보기 + 신청취소
+
 
 }
--- Back/second/src/main/java/org/project/second/groupBuy/service/GroupBuyParticipationService.java
+++ Back/second/src/main/java/org/project/second/groupBuy/service/GroupBuyParticipationService.java
@@ -1,11 +1,19 @@
 package org.project.second.groupBuy.service;
 
 import lombok.RequiredArgsConstructor;
+import org.project.second.common.enums.GroupBuyStatus;
 import org.project.second.groupBuy.domain.GroupBuy;
+import org.project.second.groupBuy.domain.GroupBuyParticipation;
+import org.project.second.groupBuy.dto.GroupBuyParticipationDto;
+import org.project.second.groupBuy.dto.GroupBuyResponseDto;
 import org.project.second.groupBuy.repository.GroupBuyParticipationRepository;
 import org.project.second.groupBuy.repository.GroupBuyRepository;
 import org.project.second.member.domain.Member;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,19 +23,103 @@ public class GroupBuyParticipationService {
     private final GroupBuyParticipationRepository groupBuyParticipationRepository;
 
     public void getApply(Long groupBuyId, Member member, int quantity) {
-        GroupBuy groupBuy = groupBuyRepository.findById(groupBuyId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 공동구매가 없습니다"));
-
-        if (member == null) {
-            throw new IllegalArgumentException("로그인이 필요한 기능입니다.");
+        //공동구매가 있는지
+        GroupBuy groupBuy = validatepost(groupBuyId);
+        //로그인 했는지
+        validateLogin(member);
+        //중복신청방지
+        if (groupBuyParticipationRepository.existsByGroupBuyAndMember(groupBuy, member)) {
+            throw new IllegalArgumentException("이미 신청한 공동구매 입니다");
         }
-        // 사람이 중복되면 안돼!
-        if ()
+        //상태별 -> 마감, 인원다참
+        if (groupBuy.getStatus() == GroupBuyStatus.CLOSED) {
+            throw new IllegalArgumentException("마감된 공동구매 입니다");
+        }
+        if (groupBuy.getStatus() == GroupBuyStatus.COMPLETED) {
+            throw new IllegalArgumentException("이미 인원이 모두 찼습니다");
+        }
+
+        GroupBuyParticipation groupBuyParticipation = GroupBuyParticipation.builder()
+                .groupBuy(groupBuy)
+                .member(member)
+                .quantity(quantity)
+                .build();
+        groupBuyParticipationRepository.save(groupBuyParticipation);
+
+        // 신청하기 누르면 1씩 증가
+        int updateParticipations = groupBuy.getCurrentParticipants() +1;
+        groupBuy.setCurrentParticipants(updateParticipations);
+
+        //신청인원이 max인원이 되면 completed 해라
+        if (updateParticipations >= groupBuy.getMaxParticipants()) {
+            groupBuy.setStatus(GroupBuyStatus.COMPLETED);
+        }
+        // 날짜가 맞으면 closed해라
+        if (groupBuy.getDeadline().isBefore(LocalDateTime.now()) &&
+        groupBuy.getStatus() == GroupBuyStatus.OPEN) {
+            groupBuy.setStatus(GroupBuyStatus.CLOSED);
+        }
+
+
+    }
+
+    //신청한사람목록보기(관리자용)
+    public List<GroupBuyParticipationDto> getApplyList(Long groupBuyId) {
+        //게시글이 있는지
+        GroupBuy groupBuy = validatepost(groupBuyId);
+
+        List<GroupBuyParticipation> participationList
+                = groupBuyParticipationRepository.findByGroupBuy(groupBuy);
+
+        return  participationList.stream().map(participation -> new GroupBuyParticipationDto(
+                participation.getMember().getId(),
+                participation.getMember().getUsername(),
+                participation.getQuantity(),
+                participation.getCreatedAt()
+        ))
+                .collect(Collectors.toList());
+
+    }
+
+    //신청취소
+    public void cancelApply(Long groupBuyId, Member member) {
+        validateLogin(member);
+        GroupBuy groupBuy = validatepost(groupBuyId);
+        GroupBuyParticipation participation = groupBuyParticipationRepository
+                .findByGroupBuyAndMember(groupBuy, member)
+                .orElseThrow(() -> new IllegalArgumentException("신청내역이 없습니다"));
+        if (LocalDateTime.now().isAfter(groupBuy.getDeadline())){
+            throw new IllegalArgumentException("마감일 이후에는 신청을 취소할 수 없습니다");
+        }
+
+        groupBuyParticipationRepository.delete(participation);
+    }
+
+    //내 신청목록만 보기
+    public List<GroupBuyResponseDto>pationList(Member member) {
+        List<GroupBuyParticipation> participationList
+                = groupBuyParticipationRepository.findByMember(member);
+
+        return participationList.stream().map(participation -> {
+            GroupBuy groupBuy = participation.getGroupBuy();
+            return GroupBuyResponseDto.from(groupBuy);
+        })
+
 
     }
 
 
 
+    public GroupBuy validatepost (Long groupBuyId) {
+        return groupBuyRepository.findById(groupBuyId)
+                .orElseThrow(() -> new IllegalArgumentException("해당공동구매가 없습니다"));
+    }
+
+    public void validateLogin(Member member){
+        if (member == null) {
+            throw new IllegalArgumentException("로그인이 필요한 기능입니다.");
+        }
+    }
 
 
 
