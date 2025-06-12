d27a010bbd388ee6e61dc40060d2ec742b4eb99e
Bumjun-hub <jc971017@gmail.com>
2025-06-12 15:13:27 +0900

    06/12 Front / back
    임시저장

--- Front/homit/src/App.js
+++ Front/homit/src/App.js
@@ -16,39 +16,39 @@ import BoardWritePage from './pages/boardpage/BoardWritePage';
 import MyPage from './pages/mypage/MyPage';
 import EditProfile from './pages/mypage/MyPageEditProfile';
 import { AuthProvider } from './utils/AuthProvider';
-
+import PaymentPage from './pages/groupbuypage/PaymentPage';
 
 function App() {
   return (
     <AuthProvider>
-      <Layout>
-        <Routes>
+      <Routes>
+        {/* 헤더/푸터 있는 페이지들 */}
+        <Route element={<Layout />}>
           <Route path="/" element={<MainPage />} />
-
           <Route path="/groupbuy" element={<GroupBuyPage />} />
           <Route path="/groupbuy/info/:id" element={<GroupBuyInfoPage />} />
           <Route path="/groupbuy/write" element={<GroupBuyWritePage />} />
           <Route path="/groupbuy/admin/edit/:id" element={<GroupBuyWritePage />} />
 
-
           <Route path="/board" element={<BoardPage />} />
           <Route path="/board/info/:category/:id" element={<BoardInfoPage />} />
           <Route path="/board/write" element={<BoardWritePage />} />
-          <Route path="/board/edit/:category/:id" element={<BoardWritePage />} />w
+          <Route path="/board/edit/:category/:id" element={<BoardWritePage />} />
 
           <Route path="/recipe" element={<RecipePage />} />
           <Route path="/RecipeInfoPage" element={<RecipeInfoPage />} />
 
           <Route path="/popular" element={<HotItemPage />} />
-          
           <Route path="/login" element={<LoginPage />} />
           <Route path="/member" element={<MemberPage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/editProfile" element={<EditProfile />} />
-        </Routes>
-      </Layout>
-    </AuthProvider>
+        </Route>
 
+        {/* 헤더/푸터 없는(단독) 페이지 */}
+        <Route path="/payment/:id" element={<PaymentPage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
