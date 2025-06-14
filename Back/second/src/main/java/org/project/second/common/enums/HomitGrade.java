24095f25ff86b99aa78fa661f53d73a8e6015da9
ansunhwa <hbo1053@naver.com>
2025-06-14 15:42:46 +0900

    enum수정

--- Back/second/src/main/java/org/project/second/common/enums/HomitGrade.java
+++ Back/second/src/main/java/org/project/second/common/enums/HomitGrade.java
@@ -1,5 +1,10 @@
 package org.project.second.common.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum HomitGrade {
     EXPERIENCE("호밋체험단", 0),   //호밋체험단
     MEMBER("호밋회원", 10),       //호밋회원
@@ -9,17 +14,4 @@ public enum HomitGrade {
 
     private final String label;
     private final int upGread;
-
-    HomitGrade(String label, int upGread){
-        this.label = label;
-        this.upGread = upGread;
-    }
-
-    public String getLabel(){
-        return label;
-    }
-
-    public int getUpGread() {
-        return upGread;
-    }
 }
