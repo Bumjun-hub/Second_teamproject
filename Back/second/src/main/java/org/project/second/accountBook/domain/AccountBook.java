0a1440e87eade9ece613ca77623c2ab707c84082
ansunhwa <hbo1053@naver.com>
2025-06-16 11:33:30 +0900

    가계부, 자산 domain

---
+++ Back/second/src/main/java/org/project/second/accountBook/domain/AccountBook.java
@@ -0,0 +1,62 @@
+package org.project.second.accountBook.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.project.second.common.domain.BaseEntity;
+import org.project.second.common.enums.ExpenseCategory;
+import org.project.second.common.enums.IncomeCategory;
+import org.project.second.common.enums.MoneyMethod;
+import org.project.second.common.enums.RecordType;
+import org.project.second.member.domain.Member;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountBook extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Long amount;  //금액
+
+    @Column(nullable = true)
+    private String memo; //메모
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RecordType recordType;  // 수입, 지출
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MoneyMethod moneyMethod;  // 수단
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private IncomeCategory incomeCategory; // 수입항목
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ExpenseCategory expenseCategory; // 지출항목
+
+    @Column(nullable = false)
+    private  boolean imRepeat; // 반복항목
+
+    @OneToMany(mappedBy = "accountBook", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AccountBookImage> accountBookImages;  // 이미지
+
+}
---
+++ Back/second/src/main/java/org/project/second/common/enums/MoneyMethod.java
@@ -0,0 +1,5 @@
+package org.project.second.common.enums;
+
+public enum MoneyMethod {
+    CASH, CHECK_CARD, CREDIT_CARD
+}
